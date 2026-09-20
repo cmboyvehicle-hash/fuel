f710626f05ba1f4a2a730a4fe3d9a032efe69e71
cmboyvehicle-hash <cmboy.vehicle@gmail.com>
2026-09-20 20:59:47 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+/* ===== ตั้งค่าเดียวที่ต้องแก้ =====
+   วางลิงก์ Web App ของ Apps Script (ลงท้ายด้วย /exec) ที่ deploy แบบ
+   "เรียกใช้ในฐานะ: ฉัน" และ "ผู้มีสิทธิ์เข้าถึง: ทุกคน"  */
+window.FUEL_API = "https://script.google.com/macros/s/AKfycbxjBZid4nd1DgtwpGxyViW0eXHT2BMtfInlTv54_OP3-oxQPrn3_h9pbDabuL9NQEOX/exec";
